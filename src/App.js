068f126425106351785c73981f126d80a83acd67
gpagatpatan <gerome.pagatpatan@mynt.xyz>
2021-05-27 17:31:37 +0800

    React Web App Project: E-commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Store from "./components/products/Store"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import ProductDetails from "./components/products/ProductDetails";
+import Cart from "./components/products/Cart";
+import { CartContext } from "./components/products/CartContext";
+import { useState } from "react";
+import NotFound from "./components/error/NotFound";
+import ReviewForm from "./components/reviews/ReviewForm";
+import Review from "./components/reviews/Review";
 
 function App() {
+
+  const [value, setValue] = useState(0);
+  const [cartItems, setCartItems] = useState([])
+  const [cartItemsPrice, setCartItemsPrice] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <CartContext.Provider value={{value, setValue, cartItems, setCartItems, cartItemsPrice, setCartItemsPrice}}>
+            <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/" />
+              <Route path="/store">
+                <Store />
+              </Route>
+              <Route path="/cart">
+                <Cart />
+              </Route>
+              <Route path="/reviews">
+                <Review />
+              </Route>
+              <Route path="/create">
+                <ReviewForm />
+              </Route>
+              <Route path="/products/:id">
+                <ProductDetails />
+              </Route>
+              <Route path="*">
+                <NotFound />
+              </Route>  
+            </Switch>
+          </div>
+        </CartContext.Provider>
+      </div>
+    </Router>
   );
+  
 }
 
 export default App;
